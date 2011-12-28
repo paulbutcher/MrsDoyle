5fb355b81837680de816400218c6e9fae389f7c7
Paul Butcher <paul@paulbutcher.com>
2011-12-28 23:06:55 +0000

    Tidy up

--- src/main/java/com/paulbutcher/HelloXmpp.scala
+++ src/main/java/com/paulbutcher/HelloXmpp.scala
@@ -1,24 +1,8 @@
 package com.paulbutcher
 
-import com.google.appengine.api.xmpp.JID;
-import com.google.appengine.api.xmpp.Message;
-import com.google.appengine.api.xmpp.MessageBuilder;
-import com.google.appengine.api.xmpp.MessageType;
-import com.google.appengine.api.xmpp.Presence;
-import com.google.appengine.api.xmpp.SendResponse;
-import com.google.appengine.api.xmpp.XMPPService;
-import com.google.appengine.api.xmpp.XMPPServiceFactory;
+import com.google.appengine.api.xmpp.{MessageBuilder, XMPPServiceFactory}
+import javax.servlet.http.{HttpServlet, HttpServletRequest, HttpServletResponse}
 
-import java.io.IOException;
-import java.util.Map;
-
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * HelloXmpp is an echo bot that sends back the message it receives.
- */
 class HelloXmpp extends HttpServlet {
 
   val xmppService = XMPPServiceFactory.getXMPPService
