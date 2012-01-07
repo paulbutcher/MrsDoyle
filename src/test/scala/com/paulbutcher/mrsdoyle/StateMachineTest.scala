7f74dfcebfdc609ce0a25ad4eda136802ed17394
Paul Butcher <paul@paulbutcher.com>
2012-01-07 01:24:57 +0000

    Remove some duplication in the tests

--- src/test/scala/com/paulbutcher/mrsdoyle/StateMachineTest.scala
+++ src/test/scala/com/paulbutcher/mrsdoyle/StateMachineTest.scala
@@ -1,13 +1,13 @@
 package com.paulbutcher.mrsdoyle
 
-import org.scalatest.WordSpec
+import org.scalatest.{WordSpec, BeforeAndAfterEach}
 import org.scalamock.generated.GeneratedMockFactory
 import org.scalamock.scalatest.MockFactory
 import org.scalamock.{CallLogging, VerboseErrors}
 
 import StateMachine._
 
-class StateMachineTest extends WordSpec with MockFactory with GeneratedMockFactory with VerboseErrors with CallLogging {
+class StateMachineTest extends WordSpec with BeforeAndAfterEach with MockFactory with GeneratedMockFactory with VerboseErrors with CallLogging {
   
   val ted = Drinker("ted")
   val dougal = Drinker("dougal")
@@ -18,13 +18,10 @@ class StateMachineTest extends WordSpec with MockFactory with GeneratedMockFacto
     "in state normal" should {
 
       "complain when it doesn't understand" in {
-        val utterances = mockObject(Utterances)
-        val messaging = mockObject(XMPPMessaging)
-
-        utterances.expects.whatDidYouSay returning "Sorry, I didn't understand that"
-        messaging.expects.send(ted, "Sorry, I didn't understand that")
-
         state = stateNormal
+
+        val messaging = mockObject(XMPPMessaging)
+        messaging.expects.send(ted, "Sorry, I didn't understand that")
         
         handle(IncomingMessage(ted, "The slithey toves did gyre, etc"))
         
@@ -32,12 +29,11 @@ class StateMachineTest extends WordSpec with MockFactory with GeneratedMockFacto
       }
       
       "start making tea when asked to" in {
-        val utterances = mockObject(Utterances)
+        state = stateNormal
+
         val messaging = mockObject(XMPPMessaging)
         val drinkers = mockObject(Drinkers)
-        
-        utterances.expects.goodIdea returning "What a great idea"
-        utterances.expects.invitation returning "Would you like to join us?"
+
         drinkers.expects.allBut(ted) returning Seq(dougal, jack)
         messaging.expects.send(ted, "What a great idea")
         messaging.expects.send(Seq(dougal, jack), "Would you like to join us?")
@@ -47,5 +43,13 @@ class StateMachineTest extends WordSpec with MockFactory with GeneratedMockFacto
         assert(state === stateMakingTea)
       }
     }
+  }
+  
+  override def beforeEach {
+    val utterances = mockObject(Utterances)
+
+    utterances.expects.whatDidYouSay returning "Sorry, I didn't understand that" anyNumberOfTimes;
+    utterances.expects.goodIdea returning "What a great idea" anyNumberOfTimes;
+    utterances.expects.invitation returning "Would you like to join us?" anyNumberOfTimes;
   }
 }
