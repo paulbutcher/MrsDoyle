367a13e364c61cb992e237b383c664062794f0bb
Paul Butcher <paul@paulbutcher.com>
2012-01-08 00:05:10 +0000

    Delete tests ready for SUG demo

--- src/test/scala/com/paulbutcher/mrsdoyle/MessagingTest.scala
+++ src/test/scala/com/paulbutcher/mrsdoyle/MessagingTest.scala
@@ -7,28 +7,9 @@ import org.scalamock.{CallLogging, VerboseErrors}
 
 import com.google.appengine.api.xmpp.{JID, Message, MessageBuilder, XMPPService}
 
-class MessagingTest extends FunSuite with MockFactory with GeneratedMockFactory with VerboseErrors with CallLogging {
+class MessagingTest extends FunSuite with MockFactory with GeneratedMockFactory {
 
   test("send") {
-    val x = mockObject(XMPP)
-    val s = mock[XMPPService]
-    val b = mock[MessageBuilder]
-    val m = mock[Message]
-    val to = Seq(Drinker("id1"), Drinker("id2"), Drinker("id3"))
-    
-    x.expects.service returning s
-    inSequence {
-      b.expects.newInstance
-      inAnyOrder {
-        b.expects.withBody("a test message") returning b
-        b.expects.withRecipientJids(where { ids: Array[JID] => 
-            ids map (_.getId) sameElements Seq("id1", "id2", "id3") 
-          }) returning b
-      }
-      b.expects.build returning m
-      s.expects.sendMessage(m)
-    }
-    
-    Messaging.send(to, "a test message")
+    //! TODO
   }
 }
--- src/test/scala/com/paulbutcher/mrsdoyle/StateMachineTest.scala
+++ src/test/scala/com/paulbutcher/mrsdoyle/StateMachineTest.scala
@@ -7,7 +7,7 @@ import org.scalamock.{CallLogging, VerboseErrors}
 
 import StateMachine._
 
-class StateMachineTest extends WordSpec with BeforeAndAfterEach with MockFactory with GeneratedMockFactory with VerboseErrors with CallLogging {
+class StateMachineTest extends WordSpec with BeforeAndAfterEach with MockFactory with GeneratedMockFactory {
   
   val ted = Drinker("ted")
   val dougal = Drinker("dougal")
@@ -18,78 +18,26 @@ class StateMachineTest extends WordSpec with BeforeAndAfterEach with MockFactory
     "in state normal" should {
 
       "complain when it doesn't understand" in {
-        state = stateNormal
-
-        val messaging = mockObject(Messaging)
-        messaging.expects.send(ted, "Sorry, I didn't understand that")
-        
-        handle(IncomingMessage(ted, "The slithey toves did gyre, etc"))
-        
-        assert(state === stateNormal)
+        //! TODO
       }
       
       "start making tea when asked to" in {
-        state = stateNormal
-
-        val messaging = mockObject(Messaging)
-        val deferred = mockObject(Deferred)
-        val drinkers = mockObject(Drinkers)
-
-        drinkers.expects.setWantsTea(ted)
-        deferred.expects.makeTeaIn(120)
-        drinkers.expects.allBut(ted) returning Seq(dougal, jack)
-        messaging.expects.send(ted, "What a great idea")
-        messaging.expects.send(Seq(dougal, jack), "Would you like to join us?")
-        
-        handle(IncomingMessage(ted, "Fancy a cuppa?"))
-        
-        assert(state === stateMakingTea)
+        //! TODO
       }
     }
     
     "in state making tea" should {
       
       "respond to a drinker saying yes" in {
-        state = stateMakingTea
-        
-        val messaging = mockObject(Messaging)
-        val drinkers = mockObject(Drinkers)
-
-        drinkers.expects.setWantsTea(dougal)
-        messaging.expects.send(dougal, "That's great news")
-        
-        handle(IncomingMessage(dougal, "Oh, yes please"))
-        
-        assert(state === stateMakingTea)
+        //! TODO
       }
       
       "select someone at random when time runs out" in {
-        state = stateMakingTea
-        
-        val messaging = mockObject(Messaging)
-        val drinkers = mockObject(Drinkers)
-        
-        drinkers.expects.chooseMaker returning dougal
-        drinkers.expects.allBut(dougal) returning Seq(ted, jack)
-        messaging.expects.send(dougal, "Well volunteered")
-        messaging.expects.send(Seq(ted, jack), " will make the tea")
-        drinkers.expects.resetWantsTea
-        
-        handle(MakeTea)
-        
-        assert(state === stateNormal)
+        //! TODO
       }
     }
   }
   
   override def beforeEach {
-    val utterances = mockObject(Utterances)
-
-    utterances.expects.whatDidYouSay returning "Sorry, I didn't understand that" anyNumberOfTimes;
-    utterances.expects.goodIdea returning "What a great idea" anyNumberOfTimes;
-    utterances.expects.invitation returning "Would you like to join us?" anyNumberOfTimes;
-    utterances.expects.greatNews returning "That's great news" anyNumberOfTimes;
-    utterances.expects.youreIt returning "Well volunteered" anyNumberOfTimes;
-    utterances.expects.willMake returning " will make the tea" anyNumberOfTimes;
   }
 }
